a699dd9a74aa993b188af8682e52dff07b2cf026
santiagoladavaz <santiagoladavaz@gmail>
2014-10-06 10:56:48 -0300

    Agregados algunas cosas

---
+++ js/club.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+/* Controllers */
+var club = angular.module('clubApp', [])
+
+
+club.controller('ClubController',function ($scope) {
+	
+	$scope.nombre = "Club";
+	
+	
+	
+	
+	
+})
