0dcc9ceffb1e3f41b4cab7ce94ee7fca350795ea
santiagoladavaz <santiagoladavaz@gmail>
2014-10-15 18:34:41 -0300

    Agregado bindigs

--- js/CarmenSanDiegoController.js
+++ js/CarmenSanDiegoController.js
@@ -1,3 +1,29 @@
-/**
- * 
- */
+'use strict';
+
+/* Controllers */
+var app = angular.module('carmenSanDiegoApp', [])
+
+app.controller('CarmenSanDiegoController', function ($scope) {
+	/* scope */
+	
+	
+	$scope.paisActual = "Argentina";
+	
+	$scope.destinosFallidos = ["Brasil","Chile","Alemania"];
+	
+	$scope.contactos = ["Brasil","Chile","Alemania"];
+	
+	$scope.villanos = [{"nombre" : "Gordo Valor"},
+	                   {"nombre":"Señor Burns"},
+	                   {"nombre":"Bonnie"},
+	                   {"nombre":"Boudou"}
+	                   ];
+    
+	$scope.miVillano = $scope.villanos[1];
+	
+	
+		
+
+	                          
+		
+});
